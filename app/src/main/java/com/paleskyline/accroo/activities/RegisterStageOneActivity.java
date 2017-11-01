b04ba18bbc6f2b619e2c8866ce075bf0f25dc243
Oscar Alston <oscar.alston@protonmail.com>
2017-11-01 23:04:47 +1100

    Added terms and privacy notice to registration activity but there is a fucking leading space for no reason that I can not figure out how to get rid of

--- app/src/main/java/com/paleskyline/accroo/activities/RegisterStageOneActivity.java
+++ app/src/main/java/com/paleskyline/accroo/activities/RegisterStageOneActivity.java
@@ -3,9 +3,11 @@ package com.paleskyline.accroo.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.paleskyline.accroo.R;
@@ -14,6 +16,7 @@ import com.paleskyline.accroo.other.Constants;
 public class RegisterStageOneActivity extends AppCompatActivity {
 
     private EditText emailAddress, confirmEmailAddress, loginPassword, confirmLoginPassword;
+    private TextView agree;
     private Button next;
 
     @Override
@@ -27,6 +30,8 @@ public class RegisterStageOneActivity extends AppCompatActivity {
         loginPassword = (EditText) findViewById(R.id.login_password);
         confirmLoginPassword = (EditText) findViewById(R.id.confirm_login_password);
         next = (Button) findViewById(R.id.next);
+        agree = (TextView) findViewById(R.id.accept_terms);
+        agree.setMovementMethod(LinkMovementMethod.getInstance());
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
